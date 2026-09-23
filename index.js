98076c204a1d2aaeb1194b7dcaa653451928a364
Praveen Kumar <raveenkumar7272@gmail.com>
2026-09-23 11:03:48 +0530

    modified index file dated 23-09-2026

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 app.listen(3000, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Express Server running on port ${PORT}`);
 });
